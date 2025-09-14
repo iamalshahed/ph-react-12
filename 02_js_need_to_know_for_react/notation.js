cb72c8780c80d3e319da170c4f53da698423655e
iamalshahed <shahedevise@gmail.com>
2025-09-15 00:46:50 +0600

    35-8 dot vs bracket notation

---
+++ 02_js_need_to_know_for_react/notation.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "Shahed",
+  age: 17,
+  friends: ["oshan", "bokchoy", "madul"],
+  salry: 15230,
+  10: "secrate code",
+};
+
+const heroName = person["friends"][1];
+console.log(heroName);
+
+const heroSecrate = person["10"];
+console.log(heroSecrate);
