def5f43cca3e0a238a914a1a71c8d01c0d82b1fc
iamalshahed <shahedevise@gmail.com>
2025-09-14 21:40:22 +0600

    35-4 Array and object Destructuring

---
+++ 02_js_need_to_know_for_react/destructuring.js
@@ -0,0 +1,15 @@
+const friends = ["Helo Alam", "Hena", "BD"];
+
+const [e1, e2, e3] = friends;
+
+// console.log(e3);
+
+const person = {
+  name: "Shahed",
+  age: 17,
+  college: "GAI",
+};
+
+const { name, age, college } = person;
+
+console.log(college);
