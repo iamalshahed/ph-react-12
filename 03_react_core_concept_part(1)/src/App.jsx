6d878b88e30610a2b2c1467c544ffd02d16c0efd
iamalshahed <shahedevise@gmail.com>
2025-09-17 01:05:41 +0600

    36-6 Read only props Two ways of conditional rendering

--- 03_react_core_concept_part(1)/src/App.jsx
+++ 03_react_core_concept_part(1)/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import ToDo from "./Todo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const time = 50;
   return (
     <>
-      <h1>Shahed</h1>
+      <h1>React Core Concepts</h1>
+      <ToDo task="Learn React" isDone={true} time={time}></ToDo>
+      <ToDo task="Revise JS" isDone={false}></ToDo>
+      <ToDo task="Take a Shower" isDone={true}></ToDo>
 
       {/* components */}
-      <Developers name="AL Shahed" tech="JavaScript"></Developers>
+      {/* <Developers name="AL Shahed" tech="JavaScript"></Developers>
 
       <Developers name="Bokchoy" tech="Python"></Developers>
 
@@ -19,8 +19,8 @@ function App() {
 
       <Player name="Mushi" runs="5462"></Player>
 
-      <Salami event='Rojar Eid' ammount='20'></Salami>
-      <Salami event='Graduation'></Salami>
+      <Salami event="Rojar Eid" ammount="20"></Salami>
+      <Salami event="Graduation"></Salami> */}
     </>
   );
 }
@@ -37,7 +37,7 @@ function Player({ name, runs }) {
   );
 }
 
-function Salami({event, ammount=0}) {
+function Salami({ event, ammount = 0 }) {
   return (
     <div>
       <p>Salami For: {event}</p>
