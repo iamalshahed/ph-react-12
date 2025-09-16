294cb6a3356a9be82d16775f20ae35e0b146c66f
iamalshahed <shahedevise@gmail.com>
2025-09-16 12:36:19 +0600

    36-5 Prop types and how to pass/read a prop

--- 03_react_core_concept_part(1)/src/App.jsx
+++ 03_react_core_concept_part(1)/src/App.jsx
@@ -1,30 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Shahed</h1>
 
       {/* components */}
-      <Developers name='AL Shahed' tech='JavaScript'></Developers>
+      <Developers name="AL Shahed" tech="JavaScript"></Developers>
 
-      <Developers name='Bokchoy' tech='Python'></Developers>
+      <Developers name="Bokchoy" tech="Python"></Developers>
+
+      <Player name="Tamim" runs="5000"></Player>
+
+      <Player name="Mushi" runs="5462"></Player>
+
+      <Salami event='Rojar Eid' ammount='20'></Salami>
+      <Salami event='Graduation'></Salami>
     </>
-  )
+  );
 }
 
-function Developers(props){
-  return(
+// const { name, runs } = { name: "Tamim", runs: '5000' };
+
+function Player({ name, runs }) {
+  // console.log(props);
+  return (
     <div>
+      <h3>Name: {name}</h3>
+      <p>Runs: {runs}</p>
+    </div>
+  );
+}
+
+function Salami({event, ammount=0}) {
+  return (
+    <div>
+      <p>Salami For: {event}</p>
+      <p>Ammount: {ammount}</p>
+    </div>
+  );
+}
+
+/*
+const { name, tech } = {
+  name: "Sabbir",
+  tech: "Java",
+};
+*/
+
+function Developers(props) {
+  return (
+    <div className="basic">
       <h3>Dev Name: {props.name}</h3>
       <p>Tech Stack: {props.tech}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
