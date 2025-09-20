1153caa2d1e540788eb730d8f14d634945887246
iamalshahed <shahedevise@gmail.com>
2025-09-20 16:59:37 +0600

    38-3 Show flags, and handle data error

--- 05_simple_react_rest_countries/src/components/Country/Country.jsx
+++ 05_simple_react_rest_countries/src/components/Country/Country.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
-const Country = () => {
+const Country = ({ country }) => {
+  const flag = {
+    width: "100px",
+    height: "auto",
+  };
+
+  const flex = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };
+
   return (
     <div>
-      <h1>Name: </h1>
+      <div style={flex}>
+        <div>
+          <img
+            style={flag}
+            src={country.flags.flags.png}
+            alt={country.flags.flags.alt}
+          />
+        </div>
+        <div className="">
+            <h3>{country.name.common}</h3>
+        </div>
+      </div>
+      <hr />
     </div>
   );
 };
