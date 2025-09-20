b6090522b0c9c4d33b81a5d4a8e6d9d0897a39a7
iamalshahed <shahedevise@gmail.com>
2025-09-20 15:07:44 +0600

    38-2 Display countries in a simple way

--- 05_simple_react_rest_countries/src/components/Countries/Countries.jsx
+++ 05_simple_react_rest_countries/src/components/Countries/Countries.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { use } from "react";
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+
   return (
     <div>
-      <h1>React JS... Rest Countries Project</h1>
+      <h2>In the countries: {countries.length}</h2>
     </div>
   );
 };
